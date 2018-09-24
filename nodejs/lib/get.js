9c6cbbbb4c46cf6ee59e8bee58edc3d6bcdfc4c9
Georgios Andrianakis <geoand@gmail.com>
2018-09-24 14:54:34 +0300

    Fix nodejs issue

--- nodejs/lib/get.js
+++ nodejs/lib/get.js
@@ -4,18 +4,19 @@ let yaml = require('js-yaml')
 var jiraClient, ymlText, yamlIssue, regex, sprints
 
 module.exports = {
-    JiraClient,
-    GetIssueById
+  JiraClient,
+  GetIssueById
 }
 
 function JiraClient(host, username, password) {
-    jiraClient = $.myJiraClient(host, username, password)
+  jiraClient = $.myJiraClient(host, username, password)
 }
 
 // Get issue by id
-function GetIssueById(id) {
+async function GetIssueById(id) {
+  try {
     var result = await getJiraIssue(id)
-    getJiraIssue(id)
+    // getJiraIssue(id)
 
     ymlText = jsontoyaml.stringify(result.fields)
     console.log(yaml.safeLoad(ymlText))
@@ -26,30 +27,36 @@ function GetIssueById(id) {
     console.log('Status      : ' + issueType.status.name)
     console.log('Type        : ' + issueType.issuetype.name)
     console.log('Author      : ' + issueType.reporter.name)
-    if (issueType.description != null) console.log('Description :\n' + issueType.description)
-    if (issueType.labels.length > 0) {
-        console.log('Labels: ', issueType.labels)
+    if (issueType.description != null) {
+      console.log(
+        'Description :\n' + issueType.description)
     }
-    ;
+    if (issueType.labels.length > 0) {
+      console.log('Labels: ', issueType.labels)
+    }
+
 
     regex = /\[(.*?)\]/
     sprints = issueType.customfield_12310940
     if (sprints != null) {
-        for (var i = 0, lengthSprints = sprints.length; i < lengthSprints; i++) {
-            // console.log("Sprint " + sprints[i]);
-            var sprint = regex.exec(sprints[i])
-            var pairs = sprint[1].split(',')
-            var map = {}
+      for (var i = 0, lengthSprints = sprints.length; i < lengthSprints; i++) {
+        // console.log("Sprint " + sprints[i]);
+        var sprint = regex.exec(sprints[i])
+        var pairs = sprint[1].split(',')
+        var map = {}
 
-            for (var j = 0, lengthPairs = pairs.length; j < lengthPairs; j++) {
-                var str = pairs[j].split('=')
-                map = $.addValueToList(map, str[0], str[1])
-            }
-            console.log('Sprint Name : ' + map['name'] + ', state : ' + map['state'])
+        for (var j = 0, lengthPairs = pairs.length; j < lengthPairs; j++) {
+          var str = pairs[j].split('=')
+          map = $.addValueToList(map, str[0], str[1])
         }
+        console.log('Sprint Name : ' + map['name'] + ', state : ' + map['state'])
+      }
     }
+  } catch (e) {
+    console.log(`Unable to get JIRA issue - Status code of error is:\n${e}`)
+  }
 }
 
 function getJiraIssue(id) {
-    return new Promise(jiraClient.issue.getIssue(id))
+  return jiraClient.issue.getIssue(id)
 }
