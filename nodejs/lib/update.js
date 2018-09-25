54deec0ff4a9a8694aa5f217265a9eaa90871d65
Charles Moulliard <cmoulliard@redhat.com>
2018-09-25 18:13:27 +0200

    Be more verbose for the status

--- nodejs/lib/update.js
+++ nodejs/lib/update.js
@@ -4,12 +4,13 @@ module.exports = {
 }
 
 var transitions = new Map()
-// New to Open (Hand Over Development)
+// New to Open status (ticket displayed within the to do column of the sprint will be now able to move to In Progress)
 transitions.set('HandOver', '791')
 // Open to Coding In Progress
 transitions.set('InProgress', '4')
 // Revert to Open
 transitions.set('StopProgress', '301')
+// Status to close/resolve the issue
 transitions.set('ResolveIssue', '5')
 transitions.set('CloseIssue', '2')
 transitions.set('LinkPR', '711')
