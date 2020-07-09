d4dd0efc50e6e8b05356380a8ec73619536928d2
Charles Moulliard <ch007m@gmail.com>
2020-07-09 15:05:40 +0200

    Verify the response generted

--- atlassian/src/test/java/dev/snowdrop/jira/MustacheTemplateTest.java
+++ atlassian/src/test/java/dev/snowdrop/jira/MustacheTemplateTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.Mustache;
 import com.github.mustachejava.MustacheFactory;
+import dev.snowdrop.jira.atlassian.model.Component;
 import dev.snowdrop.jira.atlassian.model.Release;
 import org.junit.jupiter.api.Test;
 
@@ -20,6 +21,21 @@ public class MustacheTemplateTest {
 
     private static final String YAML_FILE = "release.yaml";
     private static final String MUSTACHE_FILE = "description.mustache";
+    private String expected = "The snowdrop team is really pleased to contact you as we will release in 12 weeks a new Snowdrop BOM based on the following Spring Boot version: 2.3.0.RELEASE\n" +
+            "\n" +
+            "As product owner of the following component: Hibernate, we would like to know if you plan to release a new component for the version: 5.0.15\n" +
+            "that we will start to test the: 08/18/2020.\n" +
+            "If you don't plan to release a new component, could you please test and control if your component will continue to work with this version of spring boot\n" +
+            "We expect, in this case, that you will perform QE test and provide us a signoff !\n" +
+            "\n" +
+            "The EOL of this Snowdrop release is scheduled: September 2021\n" +
+            "\n" +
+            "We expect to get from you:\n" +
+            "\n" +
+            "- Product name and version (e.g. JWS - 5.3.1)\n" +
+            "- component & Version supported (e.g. Apache Tomcat - 9.0.30-redhat-00001)\n" +
+            "- PNC/Indy URL (e.g. http://indy.psi.redhat.com/browse/maven/...)\n" +
+            "- MRRC URL, if already released (e.g. https://maven.repository.redhat.com/ga/...)";
 
     @Test
     public void issueDescriptionTest() throws URISyntaxException, IOException {
@@ -32,11 +48,20 @@ public class MustacheTemplateTest {
 
         HashMap<String, Object> scopes = new HashMap<String, Object>();
         scopes.put("release", release);
-        scopes.put("component", release.getComponents().get(0));
+
+        Component c = release.getComponents().get(0);
+        scopes.put("component", c);
+
+        if (c.getIsStarter() == null) {
+            scopes.put("isComponent",true);
+            scopes.put("type","component");
+        } else {
+            scopes.put("isStarter",true);
+            scopes.put("type","starter");
+        }
 
         StringWriter writer = new StringWriter();
         m.execute(writer, scopes).flush();
-        assertEquals(writer.toString().contains("2.3.0.RELEASE"),true);
-        assertEquals(writer.toString().contains("Hibernate"),true);
+        assertEquals(writer.toString(),expected);
     }
 }
