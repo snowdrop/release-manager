7cd9f5adafe61a4de6a69af3008deaf4e9542e8f
Charles Moulliard <ch007m@gmail.com>
2020-07-13 16:04:13 +0200

    Add more asserts. Remove inner class to use the class of the model of the project

--- atlassian/src/test/java/dev/snowdrop/jira/ValidateYAMLtoPojoTest.java
+++ atlassian/src/test/java/dev/snowdrop/jira/ValidateYAMLtoPojoTest.java
@@ -3,10 +3,8 @@ package dev.snowdrop.jira;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import org.codehaus.jackson.annotate.JsonProperty;
+import dev.snowdrop.jira.atlassian.model.Release;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -14,8 +12,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class ValidateYAMLtoPojoTest {
     private static String YAML =
             "version: 2.3.0.RELEASE\n" +
-            "artifacts:\n" +
-            " - jira-project: https://issues.redhat.com/projects/EAPSUP/issues\n" +
+            "components:\n" +
+            " - projectKey: EAPSUP\n" +
             "   name: hibernate\n" +
             "   version: 5.0.15";
 
@@ -24,59 +22,8 @@ public class ValidateYAMLtoPojoTest {
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory()); // jackson databind
         Release release = mapper.readValue(YAML, Release.class);
         assertNotNull(release);
-        assertEquals(release.getArtifacts().get(0).getVersion(),"5.0.15");
-    }
-
-    static class Release {
-        private String version;
-        private List<Artifact> artifacts;
-
-        public String getVersion() {
-            return version;
-        }
-
-        public void setVersion(String version) {
-            this.version = version;
-        }
-
-        public List<Artifact> getArtifacts() {
-            return artifacts;
-        }
-
-        public void setArtifacts(List<Artifact> artifacts) {
-            this.artifacts = artifacts;
-        }
-
-    }
-
-    static class Artifact {
-        @JsonProperty("jira-project")
-        private String jiraProject;
-        private String name;
-        private String version;
-        public String getJiraProject() {
-            return jiraProject;
-        }
-
-        public void setJiraProject(String jiraProject) {
-            this.jiraProject = jiraProject;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getVersion() {
-            return version;
-        }
-
-        public void setVersion(String version) {
-            this.version = version;
-        }
-
+        assertEquals(release.getComponents().get(0).getVersion(),"5.0.15");
+        assertEquals(release.getComponents().get(0).getProjectKey(),"EAPSUP");
+        assertEquals(release.getComponents().get(0).getName(),"hibernate");
     }
 }
