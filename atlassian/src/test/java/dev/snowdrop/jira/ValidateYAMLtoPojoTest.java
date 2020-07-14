b82a28ff643cbc9146b7f47e02d0e495a8afa7da
Charles Moulliard <ch007m@gmail.com>
2020-07-14 07:05:45 +0200

    Add test case covering cves parsing

--- atlassian/src/test/java/dev/snowdrop/jira/ValidateYAMLtoPojoTest.java
+++ atlassian/src/test/java/dev/snowdrop/jira/ValidateYAMLtoPojoTest.java
@@ -15,15 +15,51 @@ public class ValidateYAMLtoPojoTest {
             "components:\n" +
             " - projectKey: EAPSUP\n" +
             "   name: hibernate\n" +
-            "   version: 5.0.15";
+            "   version: 5.0.15\n" +
+            " - projectKey: RESTEASY\n" +
+            "   name: RESTEasy\n" +
+            "   isStarter: true\n" +
+            "cves:\n" +
+            "  - projectKey: ENTSBT\n" +
+            "    issue: 1010\n" +
+            "  - projectKey: ENTSBT\n" +
+            "    issue: 1020";
 
     @Test
-    public void yamlToObjectTest() throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper(new YAMLFactory()); // jackson databind
+    public void checkDataOfAComponentTest() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
         Release release = mapper.readValue(YAML, Release.class);
+
         assertNotNull(release);
         assertEquals(release.getComponents().get(0).getVersion(),"5.0.15");
         assertEquals(release.getComponents().get(0).getProjectKey(),"EAPSUP");
         assertEquals(release.getComponents().get(0).getName(),"hibernate");
     }
+
+    @Test
+    public void checkIsAStarterTest() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        Release release = mapper.readValue(YAML, Release.class);
+
+        assertNotNull(release);
+        assertEquals(release.getComponents().get(1).getProjectKey(),"RESTEASY");
+        assertEquals(release.getComponents().get(1).getName(),"RESTEasy");
+        assertEquals(release.getComponents().get(1).getIsStarter(),"true");
+    }
+
+    @Test
+    public void checkCVEsTest() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        Release release = mapper.readValue(YAML, Release.class);
+
+        assertNotNull(release);
+        assertEquals(release.getCves().get(0).getProjectKey(),"ENTSBT");
+        assertEquals(release.getCves().get(0).getIssue(),"1010");
+
+        assertEquals(release.getCves().get(1).getProjectKey(),"ENTSBT");
+        assertEquals(release.getCves().get(1).getIssue(),"1020");
+    }
+
+
+
 }
